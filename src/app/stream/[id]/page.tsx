b0eb23ed03cef2e06b2107e7fdb51f296d6cf75a
dinethjanitha <djanithak@gmail.com>
2025-05-23 01:08:13 +0530

    fix bug stream page

--- src/app/stream/[id]/page.tsx
+++ src/app/stream/[id]/page.tsx
@@ -1,37 +1,21 @@
-'use client';
-
-
-// If using Next.js App Router, use `useParams`
-import { useParams } from 'next/navigation';
-// If using Next.js Pages Router, use `useRouter`
-// import { useRouter } from 'next/router';
-
- // Adjust path if needed
-import NavBarMain from '@/app/Components/NavBarMain'; // Adjust path if needed
+import React from 'react'
 import LiveStream from './LiveStream';
+import NavBarMain from '@/app/Components/NavBarMain';
 
+interface Props{
+    params : Promise<{
+        id : string
+    }>
+}
 
-const Page = () => {
-
-  const params = useParams<{ id: string }>();
-  const id = params.id;
-
-
-  if (!id) {
-    return (
-      <div>
-        <NavBarMain />
-        <p className="text-center mt-8 text-xl text-gray-600">Loading stream ID...</p>
-      </div>
-    );
-  }
-
+const page = async ({params} : Props) => {
+    const id = (await params).id;
   return (
     <div>
-      <NavBarMain />
-      <LiveStream id={id} />
+        <NavBarMain/>
+        <LiveStream id={id}/>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
